69e9702f226f7b8faa382775e72dcd997adb88fa
Redick01 <11090829@qq.com>
2024-11-17 14:22:03 +0800

    示例代码启动时exclude掉RedisAutoConfiguration

--- lightweight-rate-limiter-example/springboot-example/src/main/java/io/redick01/ratelimiter/example/Application.java
+++ lightweight-rate-limiter-example/springboot-example/src/main/java/io/redick01/ratelimiter/example/Application.java
@@ -2,11 +2,14 @@ package io.redick01.ratelimiter.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisReactiveAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 
 /**
  * @author Redick01
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {RedisAutoConfiguration.class, RedisReactiveAutoConfiguration.class, RedisRepositoriesAutoConfiguration.class})
 public class Application {
 
     /**
